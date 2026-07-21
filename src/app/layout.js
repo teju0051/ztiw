f56b22a3d947b3234288039846fe42e070dfc09b
Mr. Tejas Shinde <82798163+teju0051@users.noreply.github.com>
2026-07-21 16:09:37 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export const metadata = {
-  title: "Staff Portal HQ",
-  description: "Task management and team communication",
+  title: "ZTIW",
+  description: "ZTIW AI DIVISION OF Zen-Tech ",
 };
 
 export default function RootLayout({ children }) {
